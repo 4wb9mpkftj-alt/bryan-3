6e129825715b0f9881b7cff82fa89ee6b24dddfb
4wb9mpkftj-alt <4wb9mpkftj@privaterelay.appleid.com>
2025-11-26 21:10:39 -0600

    Delete bryan directory

--- bryan/js/runas.js
+++
@@ -1,9 +0,0 @@
-let significados = [
-    "ᚱ — Recorrer cada parte de tu ser me hace muy feliz",
-    "ᚨ — Te amo y eres el amor de toda mi vida, Bryan.",
-    "ᛞ — Cada dia que pasa estoy enamorada de ti"
-];
-
-function mostrarRuna(i) {
-    document.getElementById("textoRuna").innerText = significados[i];
-}
